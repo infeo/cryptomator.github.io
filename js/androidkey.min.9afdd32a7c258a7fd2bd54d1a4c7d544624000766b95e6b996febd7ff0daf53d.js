0a06c956aec84ec61d01dee1fc563a757c5f673c
tobihagemann <tobihagemann@users.noreply.github.com>
2021-03-23 08:13:16 +0000

    deploy: d6f5f2d15c1bbd4a24cf49e1fbbfabfd7a886c18

---
+++ js/androidkey.min.9afdd32a7c258a7fd2bd54d1a4c7d544624000766b95e6b996febd7ff0daf53d.js
@@ -0,0 +1,8 @@
+"use strict";const PADDLE_ENABLE_SANDBOX=false;const PADDLE_VENDOR_ID=39223;const ANDROID_PRODUCT_ID=578277;class AndroidLicense{constructor(form,checkoutData){this._form=form;this._checkoutData=checkoutData;this._paddle=$.ajax({url:'https://cdn.paddle.com/paddle/paddle.js',cache:true,dataType:'script'}).then(()=>{if(PADDLE_ENABLE_SANDBOX){window.Paddle.Environment.set('sandbox');}
+window.Paddle.Setup({vendor:PADDLE_VENDOR_ID});return window.Paddle;});}
+loadPrice(){this._paddle.then(paddle=>{paddle.Product.Prices(ANDROID_PRODUCT_ID,prices=>{this._checkoutData.price=prices.price.gross;});});}
+checkout(locale){if(!$(this._form)[0].checkValidity()){$(this._form).find(':input').addClass('show-invalid');return;}
+this._checkoutData.inProgress=true;this._checkoutData.errorMessage='';this._checkoutData.success=false;this._paddle.then(paddle=>{paddle.Checkout.open({product:ANDROID_PRODUCT_ID,email:this._checkoutData.email,allowQuantity:false,locale:locale,successCallback:data=>{this.onCheckoutSucceeded();this.getPaddleOrderDetails(data.checkout.id)},closeCallback:()=>{this._checkoutData.inProgress=false;}});});}
+getPaddleOrderDetails(checkoutId){this._paddle.then(paddle=>{paddle.Order.details(checkoutId,data=>{this._checkoutData.licenseKey=data.lockers[0].license_code;});});}
+onCheckoutFailed(error){this._checkoutData.success=false;this._checkoutData.errorMessage=error;this._checkoutData.inProgress=false;}
+onCheckoutSucceeded(){this._checkoutData.success=true;this._checkoutData.errorMessage='';this._checkoutData.inProgress=false;}}
